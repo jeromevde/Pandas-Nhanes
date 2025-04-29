f87fc6e4ea7daa1a9b92e53ced341a89db64c060
jeromevde <je.vanderelst@gmail.com>
2025-04-29 15:16:31 +0200

    update bumping logic

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+def get_version():
+    with open('pandas_nhanes/VERSION') as f:
+        return f.read().strip()
+
+setup(
+    name="pandas_nhanes",
+    version=get_version(),
+    packages=find_packages(),
+    install_requires=[
+        "pandas>=2.0.0",
+        "openpyxl>=3.0.0",  # for Excel support
+    ],
+    author="Jerome",
+    author_email="",
+    description="A Python API for accessing USDA FoodData Central data",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/jeromevde/pandas_nhanes",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    python_requires=">=3.8",
+    include_package_data=True,
+) 
